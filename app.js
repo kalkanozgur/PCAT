61c119f144035925177ca7347bce33e60fb62db9
Özgür Kalkan <ahmetozgurkalkan@gmail.com>
2022-07-26 23:32:59 +0300

    Photo update page

--- app.js
+++ app.js
@@ -1,21 +1,69 @@
 const express = require('express');
-const path = require('path');
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+const fileUpload = require('express-fileupload');
+
+const path = require('path');
+const fs = require('fs');
+
+const photo = require('./models/Photo'); // oluşturduğum schemayı aldım
 
 const app = express();
 
-// TEMPLATE ENGINE
-app.set('view engine', 'ejs');
-//MIDDLEWARES
-app.use(express.static('public'));
+//Database connect
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
-app.get('/', (req, res) => {
-	//res.sendFile(path.resolve(__dirname, 'views/index.html'));
-	res.render("index");
+//VİEW ENGİNE
+app.set('view engine', 'ejs');
+
+//MİDDLEWARE
+app.use(express.static('public')); // Static dosyaları koyacağımız klasörü seçtik
+app.use(express.urlencoded({ extended: true })); // Body parser
+app.use(express.json()); // Body parser
+app.use(fileUpload());
+
+//ROUTES
+app.get('/', async (req, res) => {
+  const photos = await photo.find({});
+  res.render('index', { photos });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add', (req, res) => {
+  res.render('add');
+});
+
+app.get('/photos/:photo_id', async (req, res) => {
+  const foundedPhoto = await photo.findById(req.params.photo_id);
+  res.render('photo', { photo: foundedPhoto });
+});
+
+app.post('/photos', async (req, res) => {
+  //Eğer klasör yoksa oluşturacak
+  const uploadDir = 'public/uploads';
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir);
+  }
+  // Yükeldiğimiz dosyayı yakalayıp isteiğimiz bilgileri değişkenleri aktarıyoruz
+  let uploadeImage = req.files.image;
+  let uploadPath = __dirname + '/public/uploads/' + uploadeImage.name;
+
+  // Yakaladığımız dosyayı .mv metodu ile yukarda belirlediğimiz path'a taşıyoruz. Dosya taşıma işlemi sırasında hata olmadı ise req.body ve içerisindeki image'nin dosya yolu ve adıyla beraber database kaydediyoruz
+  uploadeImage.mv(uploadPath, async (err) => {
+    if (err) console.log(err);    // Bu kısımda önemli olan add.ejs'nin içerisine form elemanı olarak encType="multipart/form-data" atribute eklemek
+    await photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadeImage.name,
+    });
+  });
+  res.redirect('/');
 });
 
 const port = 3000;
 app.listen(port, () => {
-	console.log(`Sunucu ${port} portunda başlatıldı`);
-	console.log(`http://127.0.0.1:${port}`);
+  console.log(`Sunucu ${port} portunda başlatıldı`);
+  console.log(`http://127.0.0.1:${port}`);
 });
